694855aa94994bc919b2e62e40f3cceff41d1cc1
FalseHonesty <thefalsehonesty@gmail.com>
2019-09-01 11:29:12 -0400

    Wait for command completion

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -14,6 +14,17 @@ sftp.close()
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=os.environ["SERVER_HOST"],username=os.environ["SERVER_USER"],password=os.environ["SERVER_PASS"])
-ssh_client.exec_command("rm /root/web/server.log")
-ssh_client.exec_command("killall java")
-ssh_client.exec_command("java -jar /root/web/server.jar > /root/web/server.log 2>&1 &")
+
+stdin, stdout, stderr = ssh_client.exec_command("rm /root/web/server.log")  # Non-blocking call
+stdout.channel.recv_exit_status()                                           # Blocking call
+
+stdin, stdout, stderr = ssh_client.exec_command("cd /root/web")             # Non-blocking call
+stdout.channel.recv_exit_status()                                           # Blocking call
+
+stdin, stdout, stderr = ssh_client.exec_command("killall java")             # Non-blocking call
+stdout.channel.recv_exit_status()                                           # Blocking call
+
+stdin, stdout, stderr = ssh_client.exec_command("java -jar /root/web/server.jar > /root/web/server.log 2>&1 &")
+stdout.channel.recv_exit_status()
+
+ssh_client.close()
