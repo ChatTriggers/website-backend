b116cc81af6766c59ff71edb11e13237a8891aa2
FalseHonesty <thefalsehonesty@gmail.com>
2019-09-01 02:00:08 -0400

    Fix error

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,6 +4,9 @@ import paramiko
 transport = paramiko.Transport((os.environ["SERVER_HOST"], 22))
 transport.connect(username=os.environ["SERVER_USER"], password=os.environ["SERVER_PASS"])
 sftp = paramiko.SFTPClient.from_transport(transport)
-sftp.mkdir("/root/web", ignore_existing=True)
+try:
+    sftp.mkdir("/root/web", ignore_existing=True)
+except IOError:
+    pass
 sftp.put("build/libs/" + os.listdir("build/libs/")[0], "/root/web/server.jar")
 sftp.close()
