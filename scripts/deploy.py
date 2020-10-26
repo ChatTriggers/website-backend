241e51b7c24f19e8ca72289bbbf229ce9f2c9c53
FalseHonesty <thefalsehonesty@gmail.com>
2020-10-26 19:31:39 -0400

    Build: Use utility script for restart

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -8,10 +8,7 @@ if os.environ["TRAVIS_REPO_SLUG"] != "ChatTriggers/website-backend" or os.enviro
 transport = paramiko.Transport((os.environ["SERVER_HOST"], 22))
 transport.connect(username=os.environ["SERVER_USER"], password=os.environ["SERVER_PASS"])
 sftp = paramiko.SFTPClient.from_transport(transport)
-try:
-    sftp.mkdir("/root/web")
-except IOError:
-    pass
+
 sftp.put("build/libs/" + os.listdir("build/libs/")[0], "/root/web/server.jar")
 sftp.close()
 
@@ -19,13 +16,7 @@ ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=os.environ["SERVER_HOST"],username=os.environ["SERVER_USER"],password=os.environ["SERVER_PASS"])
 
-stdin, stdout, stderr = ssh_client.exec_command("rm /root/web/server.log")  # Non-blocking call
-stdout.channel.recv_exit_status()                                           # Blocking call
-
-stdin, stdout, stderr = ssh_client.exec_command("killall java")             # Non-blocking call
-stdout.channel.recv_exit_status()                                           # Blocking call
-
-stdin, stdout, stderr = ssh_client.exec_command("cd /root/web; java -jar /root/web/server.jar --production > /root/web/server.log 2>&1 &")
+stdin, stdout, stderr = ssh_client.exec_command("/root/web/restart.sh")  # Non-blocking call
 stdout.channel.recv_exit_status()
 
 ssh_client.close()
